b4ca145d62792d787f17f57456b42342a5dd8b9f
thirdr <ryan@pimoroni.com>
2024-10-03 12:07:09 +0100

    button_test.py: visual imporvements to display output

--- examples/button_test.py
+++ examples/button_test.py
@@ -2,12 +2,24 @@
 This example shows you a simple, non-interrupt way of reading Pico Explorer's buttons with a loop that checks to see if buttons are pressed.
 """
 import time
-from pimoroni_explorer import button_a, button_b, button_c, button_x, button_y, button_z, display, WHITE, CYAN, MAGENTA, YELLOW, GREEN, BLACK
+from pimoroni_explorer import button_a, button_b, button_c, button_x, button_y, button_z, display, WHITE, CYAN, MAGENTA, YELLOW, GREEN, BLACK, BLUE, RED
+
+
+# A little function to save ourselves some time doing the drop shadows :)
+def drop_shadow_text(text, x, y, offset, wrap, size, main_colour):
+
+    # Draw the drop shadow
+    display.set_pen(BLACK)
+    display.text(text, x + offset, y + offset, wrap, size)
+
+    # Draw the main text
+    display.set_pen(main_colour)
+    display.text(text, x, y, wrap, size)
 
 
 # sets up a handy function we can call to clear the screen
 def clear():
-    display.set_pen(BLACK)
+    display.set_pen(WHITE)
     display.clear()
     display.update()
 
@@ -17,50 +29,44 @@ clear()
 display.set_font("bitmap8")
 
 while True:
-    if button_a.value() == 0:                             # if a button press is detected then...
-        clear()                                           # clear to black
-        display.set_pen(WHITE)                            # change the pen colour
-        display.text("Button A pressed", 10, 10, 240, 4)  # display some text on the screen
-        display.update()                                  # update the display
-        time.sleep(1)                                     # pause for a sec
-        clear()                                           # clear to black again
+    if button_a.value() == 0:
+        clear()
+        drop_shadow_text("Button A pressed", 65, 75, 2, 240, 4, CYAN)
+        display.update()
+        time.sleep(1)
+        clear()
     elif button_b.value() == 0:
         clear()
-        display.set_pen(CYAN)
-        display.text("Button B pressed", 10, 10, 240, 4)
+        drop_shadow_text("Button B pressed", 65, 75, 2, 240, 4, MAGENTA)
         display.update()
         time.sleep(1)
         clear()
     elif button_c.value() == 0:
         clear()
-        display.set_pen(CYAN)
-        display.text("Button C pressed", 10, 10, 240, 4)
+        drop_shadow_text("Button C pressed", 65, 75, 2, 240, 4, YELLOW)
         display.update()
         time.sleep(1)
         clear()
     elif button_x.value() == 0:
         clear()
-        display.set_pen(MAGENTA)
-        display.text("Button X pressed", 10, 10, 240, 4)
+        drop_shadow_text("Button X pressed", 65, 75, 2, 240, 4, GREEN)
         display.update()
         time.sleep(1)
         clear()
     elif button_y.value() == 0:
         clear()
-        display.set_pen(YELLOW)
-        display.text("Button Y pressed", 10, 10, 240, 4)
+        drop_shadow_text("Button Y pressed", 65, 75, 2, 240, 4, RED)
         display.update()
         time.sleep(1)
         clear()
     elif button_z.value() == 0:
         clear()
-        display.set_pen(YELLOW)
-        display.text("Button Z pressed", 10, 10, 240, 4)
+        drop_shadow_text("Button Z pressed", 65, 75, 2, 240, 4, BLUE)
         display.update()
         time.sleep(1)
         clear()
     else:
-        display.set_pen(GREEN)
-        display.text("Press any button!", 10, 10, 240, 4)
+        display.set_pen(BLACK)
+        drop_shadow_text("Press any button! :)", 65, 75, 2, 240, 4, CYAN)
         display.update()
     time.sleep(0.1)  # this number is how frequently the Pico checks for button presses
